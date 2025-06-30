59d28d22bb3baa54daab70ab1e0f5a350f0b69a8
Ma.r Garza Cedillo <mar.garzacedillo@gmail.com>
2025-06-30 12:14:23 -0600

    Ec transporte 1

--- A Walk Through the Laws of Conservation in Python/discretization_diffusion_equation.py
+++ A Walk Through the Laws of Conservation in Python/discretization_diffusion_equation.py
@@ -2,7 +2,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 import time, sys
 
-N = 41
+N = 81
 h = 2 / (N-1)
 T = 25
 k = 0.025
@@ -15,10 +15,11 @@ plt.plot(np.linspace(0,2,N),u)
 
 un = np.ones(N)
 
+nu = 1.0
 for n in range(T):
     un = u.copy()
     for i in range (1, N):
-        u[i] = un[i] - c * k / h * (un[i] - un[i-1])
+        u[i] = un[i] - 0.5* nu * (un[i]+un[i-1]) * k / h * (un[i] - un[i-1])
 
 plt.plot(np.linspace(0,2,N),u)
 plt.show()
